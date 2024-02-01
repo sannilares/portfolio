386e1041207ca1d23e1194cd98307388ead81283
Sanni Lares <sannilares@Sanni-MacBook-Pro.local>
2024-02-01 18:15:33 +0100

    Edits in footer information

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
 
     return (
         <div id="footer">
-            <p className="contact"><a href="https://www.linkedin.com/in/sannilares" className="link">LinkedIn</a> | 0400267030 | sanni.lares@aalto.fi</p>
-            <p className="copyright">Copyright Sanni Lares 2021</p>
+            <p className="contact"><a href="https://www.linkedin.com/in/sannilares" className="link">LinkedIn</a> | 0400267030 | sanni.lares@gmail.com</p>
+            <p className="copyright">Copyright Sanni Lares 2024</p>
         </div >
     );
 }
